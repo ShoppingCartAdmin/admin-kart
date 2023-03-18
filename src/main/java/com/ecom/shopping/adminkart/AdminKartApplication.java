b71a25aab96aeb892b1372c1007289a22e31a592
ShoppingCartAdmin <128209926+shoppingcartadmin@users.noreply.github.com>
2023-03-18 11:40:30 +0530

    initial commit

---
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.shopping.adminkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/shopping/adminkart/AdminKartApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.ecom.shopping.adminkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
