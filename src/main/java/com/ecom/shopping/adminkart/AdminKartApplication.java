38ac7a12e6b621b9650a5cfd299b821056c7a614
Suraj <singhsuraj1604@gmail.com>
2023-03-18 19:04:23 +0530

    with post call added category

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -1,14 +1,15 @@
 package com.ecom.shopping.adminkart;
 
 import com.ecom.shopping.adminkart.controller.ProductCategoryController;
-import com.ecom.shopping.adminkart.dto.ProductCategoryDto;
-import com.ecom.shopping.adminkart.dto.ProductDto;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MappedTypes({ProductCategoryDto.class})
+@MappedTypes(value = {ProductCategoryDto.class})
+@MapperScan("com.ecom.shopping.adminkart.mapper")
+@MappedTypes({ProductCategoryController.class})
 @MapperScan("com.ecom.shopping.adminKart.mapper")
 @SpringBootApplication
 public class AdminKartApplication {
--- src/main/java/com/ecom/shopping/adminkart/service/ProductCategoryService.java
+++ src/main/java/com/ecom/shopping/adminkart/service/ProductCategoryService.java
@@ -5,11 +5,27 @@ import com.ecom.shopping.adminkart.mapper.ProductCategoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class ProductCategoryService {
     @Autowired
+    public ProductCategoryMapper productCategoryMapper;
+    public void addCategory(ProductCategoryDto productCategoryDto) {
+        productCategoryDto.setId(generateId());
+        productCategoryDto.setCreatedDate(getDateTime());
+        productCategoryMapper.insert(productCategoryDto);
+    }
+    private String generateId() {
+       return LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddHHmmssSSS"));
+        //System.out.println(idNo);
+        //return idNo;
+    }
+   private String getDateTime() {
+      return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+
     private ProductCategoryMapper productCategoryMapper;
 
     public List<ProductCategoryDto> getAllCategory() {
