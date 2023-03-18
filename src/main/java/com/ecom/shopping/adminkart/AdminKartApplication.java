1d3f6863e4a0bd25a7958087282acf52aca6a544
amit <singhamit3738@gmail.com>
2023-03-18 18:10:10 +0530

    implimented get all category

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -3,10 +3,15 @@ package com.ecom.shopping.adminkart;
 import com.ecom.shopping.adminkart.controller.ProductCategoryController;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
+import com.ecom.shopping.adminkart.controller.ProductCategoryController;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MappedTypes({ProductCategoryDto.class})
+@MappedTypes({ProductCategoryController.class})
+@MapperScan("com.ecom.shopping.adminKart.mapper")
 @MappedTypes(value = {ProductCategoryDto.class})
 @MapperScan("com.ecom.shopping.adminkart.mapper")
 @MappedTypes({ProductCategoryController.class})
--- src/main/java/com/ecom/shopping/adminkart/dto/ProductCategoryDto.java
+++ src/main/java/com/ecom/shopping/adminkart/dto/ProductCategoryDto.java
@@ -1,5 +1,7 @@
 package com.ecom.shopping.adminkart.dto;
 
+public class ProductCategoryDto {
+    private int id;
 import java.time.LocalDateTime;
 
 public class ProductCategoryDto {
@@ -12,6 +14,12 @@ public class ProductCategoryDto {
     private String modifiedDate;
     private String deletedDate;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     public int getId() {
         return id;
     }
@@ -77,6 +85,8 @@ public class ProductCategoryDto {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
     public ProductCategoryDto setName(String name) {
         this.name = name;
         return this;
@@ -89,12 +99,32 @@ public class ProductCategoryDto {
     public ProductCategoryDto setDescription(String description) {
         this.description = description;
         return this;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getCreatedDate() {
         return createdDate;
     }
 
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(String deletedDate) {
+        this.deletedDate = deletedDate;
     public ProductCategoryDto setCreatedDate(String createdDate) {
         this.createdDate = createdDate;
         return this;
