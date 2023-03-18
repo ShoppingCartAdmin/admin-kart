b0b18d3793610e6f448c6b94f956652c2b67b112
amit <singhamit3738@gmail.com>
2023-03-18 18:10:10 +0530

    implimented get all category

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -1,6 +1,6 @@
 package com.ecom.shopping.adminkart;
 
-import com.ecom.shopping.adminkart.dto.ProductCategoryDto;
+import com.ecom.shopping.adminkart.controller.ProductCategoryController;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import com.ecom.shopping.adminkart.controller.ProductCategoryController;
@@ -9,6 +9,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MappedTypes({ProductCategoryController.class})
+@MapperScan("com.ecom.shopping.adminKart.mapper")
 @MappedTypes(value = {ProductCategoryDto.class})
 @MapperScan("com.ecom.shopping.adminkart.mapper")
 @MappedTypes({ProductCategoryController.class})
