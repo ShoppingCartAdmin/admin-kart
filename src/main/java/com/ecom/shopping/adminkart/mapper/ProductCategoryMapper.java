845de3f9d6ba2ffa9d3e57f4116e26a624cff07b
Suraj <singhsuraj1604@gmail.com>
2023-03-18 19:33:46 +0530

    with get call get category by id

--- src/main/java/com/ecom/shopping/adminkart/mapper/ProductCategoryMapper.java
+++ src/main/java/com/ecom/shopping/adminkart/mapper/ProductCategoryMapper.java
@@ -12,9 +12,14 @@ public interface ProductCategoryMapper {
     List<ProductCategoryDto> findAllCategory();
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ProductCategoryMapper {
     @Insert("insert into product_category(id,name,description,createdDate,modifiedDate,deletedDate) " + "VALUES (#{id},#{name},#{description},#{createdDate},#{modifiedDate},#{deletedDate})")
     int insert(ProductCategoryDto product_category);
+
+    ProductCategoryDto getCategory(ProductCategoryMapper productCategoryMapper);
+    @Select("SELECT * FROM product_category WHERE id = #{id}")
+    ProductCategoryDto getCategoryById(String id);
 }
