9594f0339192945c691d282a5dc166a7a53db2d9
Suraj <singhsuraj1604@gmail.com>
2023-03-19 18:18:31 +0530

    with put call update product by id

---
+++ src/main/java/com/ecom/shopping/adminkart/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.ecom.shopping.adminkart.mapper;
+
+import com.ecom.shopping.adminkart.dto.ProductDto;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.Optional;
+
+public interface ProductMapper {
+    @Select("select * from product_category Where id=#{id}")
+    Optional<ProductDto> getId(long id);
+    @Update("UPDATE product SET name=#{name},description=#{description},modifiedDate=#{modifiedDate}  WHERE id=#{id}")
+    int updateProductById(@Param("id") long id, @Param("name") String name, @Param("description") String description, @Param("modifiedDate") String modifiedDate);
+
+}
