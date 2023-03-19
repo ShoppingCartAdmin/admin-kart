ea35bb6db903e3844444d9b06ba722a8f62f0f1b
Suraj <singhsuraj1604@gmail.com>
2023-03-19 15:38:29 +0530

    with get call get category by id

--- src/main/java/com/ecom/shopping/adminkart/controller/ProductCategoryController.java
+++ src/main/java/com/ecom/shopping/adminkart/controller/ProductCategoryController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -35,8 +34,6 @@ public class ProductCategoryController {
     public void addCategory(@RequestBody ProductCategoryDto productCategoryDto) {
         productCategoryService.addCategory(productCategoryDto);
     }
-
-
     @GetMapping("/getAllCategory")
     public List<ProductCategoryDto> getAllCategory() {
         return productCategoryService.getAllCategory();
--- src/main/java/com/ecom/shopping/adminkart/dto/ProductCategoryDto.java
+++ src/main/java/com/ecom/shopping/adminkart/dto/ProductCategoryDto.java
@@ -1,8 +1,8 @@
 package com.ecom.shopping.adminkart.dto;
 
-import java.time.LocalDateTime;
 
 public class ProductCategoryDto {
+    private long id;
     private String id;
 public class ProductCategoryDto {
     private int id;
@@ -20,12 +20,13 @@ public class ProductCategoryDto {
     private String modifiedDate;
     private String deletedDate;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public ProductCategoryDto setId(long id) {
         this.id = id;
+        return this;
     public String getModifiedDate() {
         return modifiedDate;
     }
@@ -62,8 +63,9 @@ public class ProductCategoryDto {
         return name;
     }
 
-    public void setName(String name) {
+    public ProductCategoryDto setName(String name) {
         this.name = name;
+        return this;
     public ProductCategoryDto setName(String name) {
         this.name = name;
         return this;
@@ -77,15 +79,18 @@ public class ProductCategoryDto {
         this.description = description;
         return this;
     public void setDescription(String description) {
+    public ProductCategoryDto setDescription(String description) {
         this.description = description;
+        return this;
     }
 
     public String getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(String createdDate) {
+    public ProductCategoryDto setCreatedDate(String createdDate) {
         this.createdDate = createdDate;
+        return this;
     }
 
     public String getModifiedDate() {
@@ -95,8 +100,6 @@ public class ProductCategoryDto {
     public ProductCategoryDto setModifiedDate(String modifiedDate) {
         this.modifiedDate = modifiedDate;
         return this;
-    public void setModifiedDate(String modifiedDate) {
-        this.modifiedDate = modifiedDate;
     }
 
     public String getDeletedDate() {
