ec52cb1bb25241f30f144eb5f5296573a3a5cdae
Suraj <singhsuraj1604@gmail.com>
2023-03-19 18:18:31 +0530

    with put call update product by id

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -1,5 +1,6 @@
 package com.ecom.shopping.adminkart;
 
+import com.ecom.shopping.adminkart.dto.ProductDto;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import com.ecom.shopping.adminkart.controller.ProductCategoryController;
@@ -12,6 +13,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MappedTypes(value = {ProductCategoryController.class, ProductDto.class})
 @MappedTypes(value = {ProductCategoryDto.class})
 @MapperScan("com.ecom.shopping.adminkart.mapper")
 @MappedTypes({ProductCategoryDto.class})
