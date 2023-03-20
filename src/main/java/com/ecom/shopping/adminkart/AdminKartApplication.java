6c7e43a8189a0d4f1d846f050d248d9c0e9b31b6
Suraj <singhsuraj1604@gmail.com>
2023-03-20 14:08:57 +0530

    with get call get product id based on categoryId,(updated new comment)

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -1,5 +1,6 @@
 package com.ecom.shopping.adminkart;
 
+import com.ecom.shopping.adminkart.dto.ProductCategoryDto;
 import com.ecom.shopping.adminkart.dto.ProductDto;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
@@ -13,7 +14,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes(value = {ProductCategoryController.class, ProductDto.class})
+@MappedTypes(value = {ProductCategoryDto.class, ProductDto.class})
 @MapperScan("com.ecom.shopping.adminKart.mapper")
 @SpringBootApplication
 public class AdminKartApplication {
---
+++ src/main/java/com/ecom/shopping/adminkart/util/DateUtil.java
@@ -0,0 +1,10 @@
+package com.ecom.shopping.adminkart.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+    public String getDateTime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:ss"));
+    }
+}
