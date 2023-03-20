ea03e8e9780c218c2ce1803d6d0f88f4baa0cb7e
Suraj <singhsuraj1604@gmail.com>
2023-03-20 14:49:02 +0530

    with get call get product id based on categoryId,(updated new comment)

--- src/main/java/com/ecom/shopping/adminkart/controller/ProductCategoryController.java
+++ src/main/java/com/ecom/shopping/adminkart/controller/ProductCategoryController.java
@@ -16,16 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/category")
