cad4a5432283b8a6622402396a4806f279deb709
Suraj <singhsuraj1604@gmail.com>
2023-03-20 18:38:29 +0530

    with post mapping add productCategory  based on id

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -7,15 +7,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes({ProductCategoryDto.class})
-@MappedTypes(ProductCategoryDto.class)
-@MappedTypes(value = {ProductCategoryDto.class, ProductDto.class})
-@MappedTypes(value = {ProductCategoryController.class, ProductDto.class})
+
 @MappedTypes(value = {ProductCategoryDto.class})
 @MapperScan("com.ecom.shopping.adminkart.mapper")
-@MappedTypes({ProductCategoryDto.class})
-@MappedTypes({ProductCategoryController.class})
-@MapperScan("com.ecom.shopping.adminKart.mapper")
+
 @SpringBootApplication
 public class AdminKartApplication {
 
