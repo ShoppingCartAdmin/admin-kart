1eeff7870ab4d265274beb1ba020e0d08bbb48f1
Suraj <singhsuraj1604@gmail.com>
2023-03-20 16:27:18 +0530

    update product based on id

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -5,11 +5,6 @@ import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import com.ecom.shopping.adminkart.controller.ProductCategoryController;
 
-import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.annotation.MapperScan;
-import com.ecom.shopping.adminkart.dto.ProductCategoryDto;
-import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
