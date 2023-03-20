8ffad46b232a9f44e3c06b77ebae62e3b9f35792
ShoppingCartAdmin <128209926+ShoppingCartAdmin@users.noreply.github.com>
2023-03-20 19:17:35 +0530

    Update AdminKartApplication.java

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -6,7 +6,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes({ProductCategoryDto.class})
+@MappedTypes(value={ProductCategoryDto.class,ProductDto.class})
 @MapperScan("com.ecom.shopping.adminkart.mapper")
 @SpringBootApplication
 public class AdminKartApplication {
