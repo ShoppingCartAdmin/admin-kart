c94759b8218bc89674c8a63030e5ca8ba716a827
Suraj <singhsuraj1604@gmail.com>
2023-03-20 15:01:11 +0530

    with get call get product id based on categoryId,(updated new comment)

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -26,6 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MappedTypes(value = {ProductCategoryController.class, ProductDto.class})
 @MappedTypes(value = {ProductCategoryDto.class, ProductDto.class})
 @MappedTypes(ProductCategoryDto.class)
+@MappedTypes({ProductCategoryDto.class})
 @MapperScan("com.ecom.shopping.adminKart.mapper")
 @SpringBootApplication
 public class AdminKartApplication {
