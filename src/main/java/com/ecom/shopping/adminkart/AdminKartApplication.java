5acdd80eb8abe5f1ad497497aadffed9a7ed0e70
amit <singhamit3738@gmail.com>
2023-03-23 18:20:36 +0530

    getting product details

--- src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
+++ src/main/java/com/ecom/shopping/adminkart/AdminKartApplication.java
@@ -1,12 +1,13 @@
 package com.ecom.shopping.adminkart;
 
-import com.ecom.shopping.adminkart.controller.ProductCategoryController;
+import com.ecom.shopping.adminkart.dto.ProductCategoryDto;
+import com.ecom.shopping.adminkart.dto.ProductDto;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes({ProductCategoryDto.class})
+@MappedTypes(value = {ProductCategoryDto.class, ProductDto.class})
 @MapperScan("com.ecom.shopping.adminKart.mapper")
 @SpringBootApplication
 public class AdminKartApplication {
